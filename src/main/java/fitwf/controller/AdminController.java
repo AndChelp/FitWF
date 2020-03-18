d1ff869b5059d74056ba59ffdb446588a1878eb9
AndChelp <AndChelp@yandex.ru>
2020-03-18 23:04:11 +0500

    Initial

--- src/main/java/fitwf/controller/AdminController.java
+++ src/main/java/fitwf/controller/AdminController.java
@@ -1,8 +1,29 @@
 package fitwf.controller;
 
 public class AdminController {
-    /*  Admin capabilities:
-    blockUser
-    unblockUser
+    /*
+    /api/admin/
+    blockUser(int userID);
+    unblockUser(int userID);
+    deleteAllUserWF(int userID);
+    deleteAllUserLikes(int userID);
+    
+    /api/user/
+    login();
+    register(RegisterData rd);
+    changePassword(String oldPassword, String newPassword);
+    regainPassword();
+    getLikedWFs(int lastID); //returns 50 wfs
+    getFavoritedWFs(int lastID); //50 WFs
+    addWF(WF wf);
+    deleteWF(int wfID);
+    setLike(int wfID);
+    deleteLike(int wfID);
+    addToFavorites(int wfID);
+    deleteFromFavorites(int wfID);
+    
+    /api/public/
+    getWFs(int lastID); //returns 50 wfs
+    getWFsByFilter(Filter filter);
     */
 }
